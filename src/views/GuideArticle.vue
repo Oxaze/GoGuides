ccd5bfbe5aca41f47aa7d1c0b75316827afa07d7
Oxaze <julian.engst@gmail.com>
2018-10-27 20:46:55 +0200

    added parallax hero image for (guides) articles

--- src/views/GuideArticle.vue
+++ src/views/GuideArticle.vue
@@ -1,30 +1,28 @@
 <template>
-	<div class="article-wrapper">
-		<div class="guide-block-outer">
-			<div class="guide-block-inner">
-				<v-wait for="loadGuide">
-					<template slot="waiting">
-						<div>				
-							<p>Please wait...</p>
-						</div>
-					</template>
+	<div>
+		<v-wait for="loadGuide">
+			<template slot="waiting">
+				<div>				
+					<p>Please wait...</p>
+				</div>
+			</template>
 
-					<div class="hero-image">
+			<div class="article-hero-wrapper">
+				<div class="article-hero-outer">
+					<div class="article-hero-inner">
 						<h1>{{ this.content.title }}</h1>
+						<p>{{ this.content.contentType }}</p>
 					</div>
+				</div>
 
-					<div v-html="compiledMarkdown" v-if="!$wait.is('loadGuide')">></div>
-				</v-wait>
-			</div>
+			<parallax> 
+				<img v-bind:src="this.content.imageUrl" alt="Background Image">
+			</parallax>
 		</div>
 
-		<parallax :sectionHeight="120">
-			<img 
-			srcset="https://res.cloudinary.com/yrfhccre/image/upload/f_auto%2Cq_auto%2Cw_600%2Cfl_progressive/v1535831110/GoGuides/background-4k 600w,
-			https://res.cloudinary.com/yrfhccre/image/upload/f_auto%2Cq_auto:good%2Cw_900%2Cfl_progressive/v1535831110/GoGuides/background-4k 900w,
-			https://res.cloudinary.com/yrfhccre/image/upload/f_auto%2Cq_auto:best%2Cfl_progressive/v1535831110/GoGuides/background-4k 1200w"
-			src="https://res.cloudinary.com/yrfhccre/image/upload/f_auto,q_auto/v1535831110/GoGuides/background-4k" sizes="100vw" alt="Background Image">
-		</parallax>
+		<div class="article-text" v-html="compiledMarkdown" v-if="!$wait.is('loadGuide')"></div>
+	
+		</v-wait>
 	</div>
 </template> 
 
@@ -53,19 +51,11 @@ export default {
 			.then(() => {
 				this.content = this.guideContent(this.id);
 				this.$wait.end("loadGuide");
-				this.resizeParallax();
 			})
 			.catch(err => {});
 	},
-	updated() {
-		this.resizeParallax();
-	},
 	methods: {
 		...mapActions(["getEntry"]),
-		resizeParallax() {
-			const nbw = document.querySelector(".guide-block-outer").offsetHeight;
-			document.querySelector(".Masthead").style.height = `calc(${nbw}px + 4rem)`;
-		},
 	},
 	computed: {
 		...mapGetters(["guideContent"]),
