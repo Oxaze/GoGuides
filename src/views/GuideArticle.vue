618a6db0b3894c2d7aadce0b40380fdc30e4c4e8
Oxaze <julian.engst@gmail.com>
2018-10-27 18:56:09 +0200

    implemented guide-article parallax wrong

--- src/views/GuideArticle.vue
+++ src/views/GuideArticle.vue
@@ -1,24 +1,43 @@
 <template>
 	<div class="article-wrapper">
-		<v-wait for="loadGuide">
-			<template slot="waiting">
-				<div>				
-					<p>Please wait...</p>
-				</div>
-			</template>
+		<div class="guide-block-outer">
+			<div class="guide-block-inner">
+				<v-wait for="loadGuide">
+					<template slot="waiting">
+						<div>				
+							<p>Please wait...</p>
+						</div>
+					</template>
 
-			<h1>{{ this.content.title }}</h1>
-			<div v-html="compiledMarkdown" v-if="!$wait.is('loadGuide')">></div>
-		</v-wait>
+					<div class="hero-image">
+						<h1>{{ this.content.title }}</h1>
+					</div>
+
+					<div v-html="compiledMarkdown" v-if="!$wait.is('loadGuide')">></div>
+				</v-wait>
+			</div>
+		</div>
+
+		<parallax :sectionHeight="120">
+			<img 
+			srcset="https://res.cloudinary.com/yrfhccre/image/upload/f_auto%2Cq_auto%2Cw_600%2Cfl_progressive/v1535831110/GoGuides/background-4k 600w,
+			https://res.cloudinary.com/yrfhccre/image/upload/f_auto%2Cq_auto:good%2Cw_900%2Cfl_progressive/v1535831110/GoGuides/background-4k 900w,
+			https://res.cloudinary.com/yrfhccre/image/upload/f_auto%2Cq_auto:best%2Cfl_progressive/v1535831110/GoGuides/background-4k 1200w"
+			src="https://res.cloudinary.com/yrfhccre/image/upload/f_auto,q_auto/v1535831110/GoGuides/background-4k" sizes="100vw" alt="Background Image">
+		</parallax>
 	</div>
 </template> 
 
 <script>
+import Parallax from "vue-parallaxy";
 import { mapActions, mapGetters } from "vuex";
 import marked from "marked";
 
 export default {
 	name: "guide-article",
+	components: {
+		Parallax,
+	},
 	data() {
 		return {
 			id: this.$route.params.id,
@@ -34,11 +53,19 @@ export default {
 			.then(() => {
 				this.content = this.guideContent(this.id);
 				this.$wait.end("loadGuide");
+				this.resizeParallax();
 			})
 			.catch(err => {});
 	},
+	updated() {
+		this.resizeParallax();
+	},
 	methods: {
 		...mapActions(["getEntry"]),
+		resizeParallax() {
+			const nbw = document.querySelector(".guide-block-outer").offsetHeight;
+			document.querySelector(".Masthead").style.height = `calc(${nbw}px + 4rem)`;
+		},
 	},
 	computed: {
 		...mapGetters(["guideContent"]),
